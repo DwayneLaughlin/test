60da9e7072e71fd2c9a0476417aaeed94c767d7b
DwayneL <dr2@ameritech.net>
2021-10-24 16:11:35 -0500

    created variables for weight and height and bmi calculator equation

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log("it begins... again");
+const markHeightStart = 1.69;
+const markWeightStart = 78;
+const johnHeightStart = 1.95;
+const johnWeightStart = 92;
+
+const markHeightEnd = 1.88;
+const markWeightEnd = 95;
+const johnHeightEnd = 1.76;
+const johnWeightEnd = 85;
+
+const bmi = (markWeightStart / markHeightStart ** 2);
+console.log(bmi)
